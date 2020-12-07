562b4d26623c6c9b39bde17a660084543581e4df
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-07 19:09:06 +0000

    adding /urls/shortURL

--- express_server.js
+++ express_server.js
@@ -18,13 +18,25 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => { //:id means id is route parameter and available in req.param
+  console.log("req.params", req.params); //{ shortURL: 'b2xVn2' }
+  console.log("req.params.shortURL", req.params.shortURL); //{ shortURL: 'b2xVn2' }
+  const templateVars = {
+    shortURL : req.params.shortURL , //b2xVn2
+    longURL : urlDatabase[req.params.shortURL] //http://www.lighthouselabs.ca
+  };
+  res.render('urls_show', templateVars)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World!</b></body></html>\n")
 });
-
+app.get("*", (req, res) => {
+  res.statusCode(404).render("404");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
