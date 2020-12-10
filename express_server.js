021c8e056ee020af3cc1297f3569e040ff7103ca
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-10 18:30:13 +0000

    export and require helper func(refactoring)

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
-//const { generateRandomString, urlsForUser, fetchEmail, urlDatabase, users } = require("./helpers")
+const { generateRandomString, urlsForUser, fetchEmail, urlDatabase, users } = require("./helpers")
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
@@ -16,28 +16,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 const PORT = 8080;
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  // "b2xVn2": "http://www.lighthouselabs.ca",
-  // "9sm5xK": "http://www.google.com"
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
-};
-function generateRandomString() { //random id
-  let id = Math.random().toString(36).substring(2, 8);
-  return id;
-};
-const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
-}
+// const urlDatabase = {
+//   // "b2xVn2": "http://www.lighthouselabs.ca",
+//   // "9sm5xK": "http://www.google.com"
+//   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+//   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+// };
+// function generateRandomString() { //random id
+//   let id = Math.random().toString(36).substring(2, 8);
+//   return id;
+// };
+// const users = { 
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
+// };
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // 1. GET /
 app.get("/", (req, res) => {
@@ -45,15 +45,15 @@ app.get("/", (req, res) => {
 }); 
 
 //helper Function
-const urlsForUser = (id) => {
-  let urlsOfUsers = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key].userID === id) {
-      urlsOfUsers[key] = urlDatabase[key];
-    }
-  }
-  return urlsOfUsers;
-};
+// const urlsForUser = (id) => {
+//   let urlsOfUsers = {};
+//   for (let key in urlDatabase) {
+//     if (urlDatabase[key].userID === id) {
+//       urlsOfUsers[key] = urlDatabase[key];
+//     }
+//   }
+//   return urlsOfUsers;
+// };
 
 //2. GET urls
 app.get("/urls", (req, res) => {
@@ -164,17 +164,17 @@ app.get("/register", (req, res) => {
 });
 
 //helper Func: for POST Register
-const fetchEmail = (dataBase, email) => {
-  for (let key in dataBase) { //key -> id = userRandomID,user2RandomID,ed5f5p(vase newUser)  
-    //console.log("key of 88:", key);
-    //console.log("dataBase[key] of 88:", dataBase[key]);//{ id: 'mb3dt1', email: 'amerimahsa@yahoo.com', password: '123' }
-    if (dataBase[key].email === email) {
-      //return dataBase[key].email;
-      return dataBase[key]; //{id: ..., email: ..., pass: ...}
-      //return true;
-    }
-  } return false;
-};
+// const fetchEmail = (dataBase, email) => {
+//   for (let key in dataBase) { //key -> id = userRandomID,user2RandomID,ed5f5p(vase newUser)  
+//     //console.log("key of 88:", key);
+//     //console.log("dataBase[key] of 88:", dataBase[key]);//{ id: 'mb3dt1', email: 'amerimahsa@yahoo.com', password: '123' }
+//     if (dataBase[key].email === email) {
+//       //return dataBase[key].email;
+//       return dataBase[key]; //{id: ..., email: ..., pass: ...}
+//       //return true;
+//     }
+//   } return false;
+// };
 
 //3.2 POST Register
 app.post("/register", (req, res) => {
